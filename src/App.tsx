728f58fb61a62e72f02eb53961a7918fcb4eb1df
Artemij Kondurov <kondurovartemij@yandex.ru>
2025-01-28 16:28:48 +0300

    Changed error base page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css'
 import Menu from './components/Menu';
-import HomePage from './components/HomePage';
-import DriftPage from './components/DriftPage';
-import TimeAttackPage from './components/TimeAttackPage';
-import ForzaPage from './components/ForzaPage';
+import HomePage from './pages/HomePage';
+import DriftPage from './pages/DriftPage';
+import TimeAttackPage from './pages/TimeAttackPage';
+import ForzaPage from './pages/ForzaPage';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/ra16-homeworks-router-menu'>
       <div>
         <Menu />
         <div className="page">
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,12 +1,20 @@
 import { NavLink } from 'react-router-dom';
 
+import pagesInfo from '../../pagesInfo.json'
+
 function Menu() {
+
     return (
         <nav className="menu">
-            <NavLink className={({isActive}) => isActive ? "menu__item menu__item-active": "menu__item"} to="/">Главная</NavLink>
-            <NavLink className={({isActive}) => isActive ? "menu__item menu__item-active": "menu__item"} to="/drift">Дрифт-такси</NavLink>
-            <NavLink className={({isActive}) => isActive ? "menu__item menu__item-active": "menu__item"} to="/timeattack">Time Attack</NavLink>
-            <NavLink className={({isActive}) => isActive ? "menu__item menu__item-active": "menu__item"} to="/forza">Forza Karting</NavLink>
+            {pagesInfo.map(page => (
+                <NavLink
+                    className={({ isActive }) => isActive ? "menu__item menu__item-active" : "menu__item"}
+                    key={page.to}
+                    to={page.to}
+                >
+                    {page.title}
+                </NavLink>
+            ))}
         </nav>
     );
 }
